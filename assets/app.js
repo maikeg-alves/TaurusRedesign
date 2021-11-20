d4f3a2fcb58965ffcfbc25c5384456eea754ce25
Maicon Gabriel Alves <maicongabrielalves99@gmail.com>
2021-11-20 14:49:11 -0300

    update 20/11 : atualizei o carrinho para um sistema de armazenagem de dados em array , isso torna mais facil a criação de novos produtos e tbm para um futuro painel administrativo, com isso refix o script do carrinho de compras e contador de itens na lista, o sitema de acionar mais quantidades do produto apartir do carrinho ainda esta em produção e vai levar mais tempo, o codigo apresenta ainda muito codigo lixo sem utilidade mas ainda não vou focar em fazer essa limpeza, agora vou focar no adiconador de quantidade depois passo para o login e em seguida para o finalizador de compras.

--- assets/app.js
+++ assets/app.js
@@ -1,346 +1,293 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+var script = document.createElement("script");
+script.src = "https://code.jquery.com/jquery-3.4.1.min.js";
+script.type = "text/javascript";
+document.getElementsByTagName("head")[0].appendChild(script);
 
-var X,Y;
+var X, Y;
 var Xtratado, Ytratado;
 
-function mover()
-{
-  traslate = 'translate('+Xtratado+'px,'+Ytratado+'px)';
-  
+function mover() {
+  traslate = "translate(" + Xtratado + "px," + Ytratado + "px)";
+
   $(".fundo").css({
-    'transform': traslate
+    transform: traslate,
   });
-  
-  window.requestAnimationFrame(mover)
+
+  window.requestAnimationFrame(mover);
 }
 
-$( document ).on( "mousemove", function( event ) {
+$(document).on("mousemove", function (event) {
   X = event.pageX;
   Y = event.pageY;
-  
+
   ancho = $(window).width() / 2;
   largo = $(window).height() / 2;
-  
-  Xtratado =  (ancho - X)*(1/50);
-  Ytratado =  (largo - Y)*(1/50);
 
+  Xtratado = (ancho - X) * (1 / 50);
+  Ytratado = (largo - Y) * (1 / 50);
 });
 
 mover();
 
 /* carossel */
 
-window.onload = function() {
-	recCarouselEvents();
-}
+window.onload = function () {
+  recCarouselEvents();
+};
 
 function recCarouselEvents() {
+  // RECOMMENDATION CAROUSELS
+  var recTranslate = 0;
+  var recTranslateIncrem = 904;
+  var carRecsItems = $(".carousel-recs .carousel-listitem");
+  var carRecsItem = $(carRecsItems[0]);
+  var carRecsItemWidth = carRecsItem.outerWidth(true);
+  var carRecsRowWidth = carRecsItemWidth * 4;
+  var carRecsMaxRowWidth = carRecsItemWidth * carRecsItems.length;
 
-	// RECOMMENDATION CAROUSELS
-	var recTranslate = 0;
-	var recTranslateIncrem = 904;
-	var carRecsItems = $('.carousel-recs .carousel-listitem');
-	var carRecsItem = $(carRecsItems[0]);
-	var carRecsItemWidth = carRecsItem.outerWidth(true);
-	var carRecsRowWidth = (carRecsItemWidth * 4);
-	var carRecsMaxRowWidth = (carRecsItemWidth * carRecsItems.length);
+  $(".carousel-prev-rec").click(function () {
+    // PREV RECS
+    carRecsList = $(this)
+      .parents(".carousel-header")
+      .siblings(".carousel-body")
+      .find(".carousel-list");
+    recTranslate += recTranslateIncrem;
 
-	$('.carousel-prev-rec').click(function() {
-		// PREV RECS
-		carRecsList = $(this).parents('.carousel-header')
-			.siblings('.carousel-body').find('.carousel-list');
-		recTranslate += recTranslateIncrem;
+    if (recTranslate <= 0) {
+      carRecsList.css("transform", "translatex(" + recTranslate + "px)");
+    } else {
+      recTranslate = 0;
+    }
+  });
 
-		if (recTranslate <= 0) {
-			carRecsList.css('transform', 'translatex(' + recTranslate + 'px)');
-		} else {
-			recTranslate = 0;
-		}
-	})
+  $(".carousel-next-rec").click(function () {
+    // NEXT RECS
+    carRecsList = $(this)
+      .parents(".carousel-header")
+      .siblings(".carousel-body")
+      .find(".carousel-list");
+    recTranslate -= recTranslateIncrem;
 
-	$('.carousel-next-rec').click(function() {
-		// NEXT RECS
-		carRecsList = $(this).parents('.carousel-header')
-			.siblings('.carousel-body').find('.carousel-list');
-		recTranslate -= recTranslateIncrem;
-
-		if (recTranslate >= -1808) {
-			carRecsList.css('transform', 'translatex(' + recTranslate + 'px)');
-		} else {
-			recTranslate = -1808;
-		}
-
-	});
+    if (recTranslate >= -1808) {
+      carRecsList.css("transform", "translatex(" + recTranslate + "px)");
+    } else {
+      recTranslate = -1808;
+    }
+  });
 }
 /* menu scroll fix  */
 
-window.onscroll = function(){
-	const menu = document.querySelector('.fixed-top')
-	var top = window.pageYOffset || document.documentElement.scrollTop
-	if (top > 130) {
-		menu.classList.add('teste')
-		console.log ('maior que 300')
-	} else {
-		menu.classList.remove('teste')
-	}
+window.onscroll = function () {
+  const menu = document.querySelector(".fixed-top");
+  var top = window.pageYOffset || document.documentElement.scrollTop;
+  if (top > 130) {
+    menu.classList.add("teste");
+    console.log("maior que 300");
+  } else {
+    menu.classList.remove("teste");
+  }
+};
+
+/*     CARRINHO DE COMPRAS  */
+
+/* array com os produtos  */
+const items = [
+  {
+    id: 0,
+    nome: "Arma 1",
+    img: "/img/frenzy.png",
+    quantidade: 0,
+    delalhes:
+      "After generating your fancy text symbols, you can copy and paste the to most websites and text processors. You could use it to generate a fancy Agario name (yep, weird text.",
+    price: 10,
+    },
+  {
+    id: 1,
+    nome: "Arma 2",
+    img: "/img/classic.png",
+    quantidade: 0,
+    delalhes:
+      "After generating your fancy text symbols, you can copy and paste the to most websites and text processors. You could use it to generate a fancy Agario name (yep, weird text.",
+    price: 99,
+    },
+  {
+    id: 2,
+    nome: "Arma 3",
+    img: "/img/shorty.png",
+    quantidade: 0,
+    price: 99,
+    delalhes:
+      "After generating your fancy text symbols, you can copy and paste the to most websites and text processors. You could use it to generate a fancy Agario name (yep, weird text.",
+  },
+  {
+    id: 3,
+    nome: "Arma 4",
+    img: "/img/sheriff.png",
+    quantidade: 0,
+    price: 99,
+    delalhes:
+      "After generating your fancy text symbols, you can copy and paste the to most websites and text processors. You could use it to generate a fancy Agario name (yep, weird text.",
+  },
+  {
+    id: 4,
+    nome: "Arma 5",
+    img: "/img/CA762IA2-dir.png",
+    quantidade: 0,
+    price: 99,
+    delalhes:
+      "After generating your fancy text symbols, you can copy and paste the to most websites and text processors. You could use it to generate a fancy Agario name (yep, weird text.",
+  },
+  {
+    id: 5,
+    nome: "Arma 6 ",
+    img: "/img/phantom-article.png",
+    quantidade: 0,
+    price: 99,
+    delalhes:
+      "After generating your fancy text symbols, you can copy and paste the to most websites and text processors. You could use it to generate a fancy Agario name (yep, weird text.",
+  },
+  {
+    id: 6,
+    nome: "Arma 7 ",
+    img: "/img/judge.png",
+    quantidade: 0,
+    price: 99,
+    delalhes:
+      "After generating your fancy text symbols, you can copy and paste the to most websites and text processors. You could use it to generate a fancy Agario name (yep, weird text.",
+  },
+  {
+    id: 7,
+    nome: "Arma 8 ",
+    img: "/img/ump45.png",
+    quantidade: 0,
+    price: 99,
+    delalhes:
+      "After generating your fancy text symbols, you can copy and paste the to most websites and text processors. You could use it to generate a fancy Agario name (yep, weird text.",
+  },
+];
+productsInCart = [];
+
+/* inicializador da loja  */
+inicializarLoja = () => {
+  var containerProdutos = document.getElementById("produtos");
+  items.map((val) => {
+    $(".cart").css({
+      visibility: "hidden",   
+    });
+    console.log(val.nome);
+    containerProdutos.innerHTML += `
+                <li class="coll-3 p-3">
+                  <section class="banner">
+                    <h1 class="pt-3 text-center">${val.nome}</h1>
+                    <img class="py-5 px-4" src="${val.img}">
+                    <p class="px-4 fs-6">T4 11,5 (FULL-AUTO)</p>
+                    <p class="px-4">Detalhes</p>
+                    <p class="c pb-5 px-4 text-break"> ${val.delalhes} </p>
+                    <div class="d-flex align-items-center justify-content-evenly">
+                      <button key="${val.id}" onclick="contar()" class="add-to-cart btn btn-green btn-primary rounded-pill" type="button">Comprar</button>
+                      <button data-name="arma 8" data-price="100" class="add-to-cart addtocart btn btn-primary rounded-pill btn-white " type="button"><i
+                          class="fas fa-plus"></i></button>
+                    </div>
+                  </section>
+                </li>
+                
+                `;
+  });
+};
+
+inicializarLoja();
+
+/*       carrinho  */
+atualizarCarrinho = () => {
+  var containerCarrinho = document.querySelector(".modal-body");
+  var valorTotaldosProdutos = document.getElementById("totalproduct")
+  limpartudo = () => {
+    valorTotaldosProdutos.innerHTML = "";
+    containerCarrinho.innerHTML = "";
+  }
+  limpartudo()
+  items.map((val) => {
+    if (val.quantidade > 0) {
+      var totalR =  val.price * val.quantidade 
+      containerCarrinho.innerHTML += `
+      <div class="d-flex"> 
+      <img src="${val.img}" class="col-auto cartImg"> 
+      <p class="px-3"> ${val.nome} </p> 
+        <div> 
+        <input type="button" class="plus" onclick="menos()" value="-">
+          ${val.quantidade}
+        <input type="button" onclick="mais()" value="+">
+        </div>
+        <p class="total-item px-3 "> R$ ${totalR}</p>       
+      </div>
+      <hr>`;
+
+      var total = items.reduce(getTotal, 0);
+
+      function getTotal(total, item) {
+       return total + (item.price * item.quantidade);
+      }
+   /*    $('.btn').addEventListener('click', function(){
+        valorTotaldosProdutos.innerHTML= ""
+      }) */
+      valorTotaldosProdutos.innerHTML = `<p> Valor Total: R$ ${total} </p>`
+    }
+  });
+  
+};
+
+/* contador total de itens  */
+var numero = 0;
+var contador = document.querySelector(".total-count");
+
+function contar() {
+  ++numero;
+  contador.innerHTML = numero;
 }
 
-/* $('.addtocart').on('click',function(){
-	console.log('teste')
-}) */
+function zerar() {
+   numero = 0
+   contador.innerHTML = null
+   limpartudo()
+   removeboll()
+ } 
 
-/* $(document).ready(function(){
-	$('.addtocart').on('click',function(){
-	  console.log('apertou')
-	  var button = $(this);
-	  var cart = $('#cart');
-	  var cartTotal = cart.attr('data-totalitems');
-	  var newCartTotal = parseInt(cartTotal) + 1;
-	  
-	  button.addClass('sendtocart');
-	  setTimeout(function(){
-		button.removeClass('sendtocart');
-		cart.addClass('shake').attr('data-totalitems', newCartTotal);
-		setTimeout(function(){
-		  cart.removeClass('shake');
-		},500)
-	  },1000)
-	})
+ removeboll = () => {
+ if (numero > 0 ) {
+  $(".cart").css({
+    visibility: "visible",
+  });
+  }else {
+  $(".cart").css({
+    visibility: "hidden",   
+  });
+  }
+}
+
+/*  quantidade  */
+var links = document.getElementsByClassName("btn");
+
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function () {
+    let key = this.getAttribute("key");
+    items[key].quantidade++;
+    removeboll()
+    atualizarCarrinho();
+  });
+}
+atualizarCarrinho();
+
+/* function totalCost(items) {
+  // console.log("The product price is", product.price);
+   items.map((val)=>{
+   let cartCost = val.quantidade
+   if(cartCost != null) {
+       cartCost parseInt(cartCost);
+       var totaal = ( cartCost +
+       val.price);
+    }else {
+      var totaal = val.price);
+   }
   })
- */
-/*   cart script  */
-
- // ************************************************
-      // Shopping Cart APIas
-      // ************************************************
-
-      var shoppingCart = (function () {
-        // =============================
-        // Private methods and propeties
-        // =============================
-
-
-        /* o array responsavel por gurdar os valores adiconados ao carrinho  */
-        cart = [];
-
-        // Constructor
-/*         aqui ele vai procurar pelas palavras name, prince e count para 
-        quando eu clicar no add cart ele acionar o nome, valor e a conta de todos juntos 
-        sem precisar adicionar os procutos em um aray com id */ 
-        function Item(name, price, count) {
-          this.name = name;
-          this.price = price;
-          this.count = count;
-        }
-
-        // Save cart   
-        function saveCart() {
-          sessionStorage.setItem("shoppingCart", JSON.stringify(cart));
-        }
-
-        // Load cart
-        function loadCart() {
-          cart = JSON.parse(sessionStorage.getItem("shoppingCart"));
-        }
-        if (sessionStorage.getItem("shoppingCart") != null) {
-          loadCart();
-        }
-
-        // =============================
-        // Public methods and propeties
-        // =============================
-        var obj = {};
-
-        // Add to cart
-        obj.addItemToCart = function (name, price, count) { 
-          for (var item in cart) {
-            if (cart[item].name === name) {
-              cart[item].count++;
-              saveCart();
-              return;
-            }
-          }
-          var item = new Item(name, price, count);
-          cart.push(item);
-          saveCart();
-        };
-        // Set count from item
-        obj.setCountForItem = function (name, count) {
-          for (var i in cart) {
-            if (cart[i].name === name) {
-              cart[i].count = count;
-              break;
-            }
-          }
-        };
-        // Remove item from cart
-        obj.removeItemFromCart = function (name) {
-          for (var item in cart) {
-            if (cart[item].name === name) {
-              cart[item].count--;
-              if (cart[item].count === 0) {
-                cart.splice(item, 1);
-              }
-              break;
-            }
-          }
-          saveCart();
-        };
-
-        // Remove all items from cart
-        obj.removeItemFromCartAll = function (name) {
-          for (var item in cart) {
-            if (cart[item].name === name) {
-              cart.splice(item, 1);
-              break;
-            }
-          }
-          saveCart();
-        };
-
-        // Clear cart
-        obj.clearCart = function () {
-          cart = [];
-          saveCart();
-        };
-
-        // Count cart
-        obj.totalCount = function () {
-          var totalCount = 0;
-          for (var item in cart) {
-            totalCount += cart[item].count;
-          }
-          return totalCount;
-        };
-
-        // Total cart
-        obj.totalCart = function () {
-          var totalCart = 0;
-          for (var item in cart) {
-            totalCart += cart[item].price * cart[item].count;
-          }
-          return Number(totalCart.toFixed(2));
-        };
-
-        // List cart
-        obj.listCart = function () {
-          var cartCopy = [];
-          for (i in cart) {
-            item = cart[i];
-            itemCopy = {};
-            for (p in item) {
-              itemCopy[p] = item[p];
-            }
-            itemCopy.total = Number(item.price * item.count).toFixed(2);
-            cartCopy.push(itemCopy);
-          }
-          return cartCopy;
-        };
-
-        // cart : Array
-        // Item : Object/Class
-        // addItemToCart : Function
-        // removeItemFromCart : Function
-        // removeItemFromCartAll : Function
-        // clearCart : Function
-        // countCart : Function
-        // totalCart : Function
-        // listCart : Function
-        // saveCart : Function
-        // loadCart : Function
-        return obj;
-      })();
-
-      // *****************************************
-      // Triggers / Events
-      // *****************************************
-      // Add item
-      $(".add-to-cart").click(function (event) {
-        event.preventDefault();
-        var name = $(this).data("name");
-        var price = Number($(this).data("price"));
-        shoppingCart.addItemToCart(name, price, 1);
-        displayCart();
-      });
-
-       if ( itemCopy.count === "1" ) {
-         console.error('teste')
-       } 
-      // Clear items
-      $(".clear-cart").click(function () {
-        shoppingCart.clearCart();
-        displayCart();
-      });
-
-      function displayCart() {
-        var cartArray = shoppingCart.listCart();
-        var output = "";
-        for (var i in cartArray) {
-          output +=
-            "<tr>" +
-            "<td>" +
-            cartArray[i].name +
-            "</td>" +
-            "<td>(" +
-            cartArray[i].price +
-            ")</td>" +
-            "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" +
-            cartArray[i].name +
-            ">-</button>" +
-            "<input type='number' class='item-count form-control' data-name='" +
-            cartArray[i].name +
-            "' value='" +
-            cartArray[i].count +
-            "'>" +
-            "<button class='plus-item btn btn-primary input-group-addon' data-name=" +
-            cartArray[i].name +
-            ">+</button></div></td>" +
-            "<td><button class='delete-item btn btn-danger' data-name=" +
-            cartArray[i].name +
-            ">X</button></td>" +
-            " = " +
-            "<td>" +
-            cartArray[i].total +
-            "</td>" +
-            "</tr>";
-        }
-        $(".show-cart").html(output);
-        $(".total-cart").html(shoppingCart.totalCart());
-        $(".total-count").html(shoppingCart.totalCount());
-      }
-
-      // Delete item button
-
-      $(".show-cart").on("click", ".delete-item", function (event) {
-        var name = $(this).data("name");
-        shoppingCart.removeItemFromCartAll(name);
-        displayCart();
-      });
-
-      // -1
-      $(".show-cart").on("click", ".minus-item", function (event) {
-        var name = $(this).data("name");
-        shoppingCart.removeItemFromCart(name);
-        displayCart();
-      });
-      // +1
-      $(".show-cart").on("click", ".plus-item", function (event) {
-        var name = $(this).data("name");
-        shoppingCart.addItemToCart(name);
-        displayCart();
-      });
-
-      // Item count input
-      $(".show-cart").on("change", ".item-count", function (event) {
-        var name = $(this).data("name");
-        var count = Number($(this).val());
-        shoppingCart.setCountForItem(name, count);
-        displayCart();
-      });
-
-      displayCart();
-      //# sourceURL=pen.js
-
+  }
+  totalCost() */
